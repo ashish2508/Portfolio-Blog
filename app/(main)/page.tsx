1f309957b2abc04d7a4ce4bbb06ecbdd59c9c1ac
ashish2508 <jha250805@gmail.com>
2025-05-13 19:38:38 +0530

    Personal Portfolio and blogging

---
+++ app/(main)/page.tsx
@@ -0,0 +1,17 @@
+import { HeroSection } from './_componets/hero'
+import { InformationSection } from './_componets/information'
+import { Introduction } from './_componets/introduction'
+
+export default function HomePage() {
+  return (
+    <main>
+      <h1 className="sr-only">{"Ashish's Portfolio"}</h1>
+
+      <HeroSection />
+
+      <Introduction />
+
+      <InformationSection />
+    </main>
+  )
+}
---
+++ lib/metadata.ts
@@ -0,0 +1,84 @@
+import type { Metadata as NextMetadata } from 'next'
+
+type Metadata = Omit<NextMetadata, 'title' | 'keywords'> & {
+  title: string
+  keywords: string[]
+}
+
+export const createMetadata = (override: Partial<Metadata> = {}): Metadata => {
+  const siteName = 'Ashish'
+  const title = override.title ? `${override.title} | ${siteName}` : siteName
+  const description =
+    override.description ??
+    `I'm Ashish, a web developer specializing in Next.js. Passionate about creating efficient web applications and anime enthusiast.`
+  const {
+    title: _,
+    description: __,
+    keywords = [],
+    openGraph,
+    ...restOverride
+  } = override
+  const { images: ogImages, url: ogUrl, ...restOpenGraph } = openGraph ?? {}
+  const url = `${getBaseUrl()}${ogUrl ?? ''}`
+
+  return {
+    metadataBase: new URL(getBaseUrl()),
+    applicationName: siteName,
+    title,
+    description,
+    category: 'Portfolio',
+    authors: { name: 'Ashish', url: getBaseUrl() },
+    manifest: `${getBaseUrl()}/manifest.webmanifest`,
+    keywords: [
+      ...keywords,
+      'Ashish',
+      'ashish2508',
+      'Ashish Jha',
+      'web development',
+      'frontend',
+      'Next.js',
+      'React',
+      'TypeScript',
+      'portfolio',
+      'developer',
+      'blogs',
+      'Rias Gremory',
+    ],
+    openGraph: {
+      url,
+      title,
+      description,
+      siteName,
+      type: 'website',
+      images: [
+        { url: '/api/og?uwu=true', alt: 'Ashish' },
+        ...(Array.isArray(ogImages) ? ogImages : ogImages ? [ogImages] : []),
+      ],
+      ...restOpenGraph,
+    },
+    twitter: {
+      card: 'summary_large_image',
+      creatorId: '@ashish2508',
+      ...override.twitter,
+    },
+    icons: {
+      icon: '/favicon.ico',
+      shortcut: '/favicon-16x16.png',
+      apple: '/apple-touch-icon.png',
+    },
+    alternates: {
+      canonical: url,
+      ...override.alternates,
+    },
+    assets: '/assets',
+    ...restOverride,
+  }
+}
+
+export function getBaseUrl() {
+  if (typeof window !== 'undefined') return window.location.origin
+  if (process.env.VERCEL_PROJECT_PRODUCTION_URL)
+    return `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`
+  return `http://localhost:${process.env.PORT ?? 3000}`
+}
