eecfb54f108847e557fda5e095481d8e2a46c73a
ashish jha <jha250805@gmail.com>
2025-07-08 22:17:59 +0530

    img working

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,13 @@
-import type { NextConfig } from 'next'
-import { createMDX } from 'fumadocs-mdx/next'
+import type { NextConfig } from 'next';
+import { createMDX } from 'fumadocs-mdx/next';
 
 const withMDX = createMDX()
 
 const config = {
-  images: { remotePatterns: [{ protocol: 'https', hostname: 'gravatar.com' }] },
-
+  images: {
+    unoptimized: true, 
+    remotePatterns: [{ protocol: 'https', hostname: 'gravatar.com' }],
+  },
   reactStrictMode: true,
   typescript: { ignoreBuildErrors: true },
   eslint: { ignoreDuringBuilds: true },
